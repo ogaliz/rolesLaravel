ff03ed781381c07bf955e0e756859d4eacb097ae
oscar.galvez <oscar.galvez@avannubo.com>
2018-09-19 10:05:33 +0200

    Se han añadido todas las rutas tanto para usuarios, roles y productos

--- roles/resources/views/welcome.blade.php
+++ roles/resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Roles y permisos
                 </div>
 
                 <div class="links">
